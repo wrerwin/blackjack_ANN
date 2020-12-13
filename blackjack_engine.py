a932d948e9fc67304330259f62055a3114bacdda
Casey Brock <19877843+caseynbrock@users.noreply.github.com>
2020-12-12 22:51:46 -0500

    Add blackjack logic to engine

--- blackjack_engine.py
+++ blackjack_engine.py
@@ -1,31 +1,84 @@
-example_hand = [1,3,12]  # ace, 3, queen
-
-def is_busted(hand):
-    """returns True if hand is busted, False otherwise"""
-    #  if total(hand) > 21:
-    #      return True
-    #  need to deal with aces
-    pass
+import random
 
 class Deck():
-    def __init__(self, n=1):
-        self.cards = self._random_deck(n)
+    """currently initializes as a random deck of 52 cards"""
+    def __init__(self):
+        self.cards = self._random_deck()
 
-    def draw_card(self):
-        card = self.cards.pop(0)
-        return card
+    def draw_card(self, n=1):
+        """return single card (int) or list of cards (list of ints)"""
+        if n==1:
+            card = self.cards.pop(0)
+            return card
+        if n>1:
+            return [self.draw_card() for x in range(n)] 
 
-    def _random_deck():
+    def _random_deck(self):
         # generate random deck of n*52 cards
-        pass
+        deck = 4*[*range(1,14)]
+        random.shuffle(deck)
+        return deck
+
+class Hand():
+    def __init__(self, cards=None):
+        if cards == None:
+            self.cards = []
+        else:
+            self.cards=cards
+
+    def sum_cards(self):
+        # need to account for aces
+        pass 
+
+    def is_busted(self):
+        """kinda unnecessary but whatever"""
+        return self.sum_cards() > 21
 
 def blackjack_round():
-    deck = Deck()
-    player_hand = [deck.draw_card() for x in range(2)]
-    dealer_hand = [deck.draw_card() for x in range(2)]
-    dealer_upcard = dealer_hand[1]
+    """ 
+    Results:
+        lose: -1
+        push: 0
+        win: 1
+        blackjack: 1.5
+    """
+    deck, player_hand, dealer_hand = generate_initial_game_state()
+    dealer_upcard = dealer_hand[1]  # dealer's second card
+
+    # check if player or dealer have blackjack
+    player_blackjack = dealer_blackjack = False
+    if player_hand.sum_cards == 21:
+        player_blackjack = True
+    if dealer_hand.sum_cards == 21:
+        dealer_blackjack = True
+    if player_blackjack and dealer_blackjack:
+        return 0
+    elif player_blackjack:
+        return 1.5
+    elif dealer_blackjack:
+        return -1
+
     # loop through player decisions until stay or bust
+    ### player decision code or function goes here
+    if player_hand.is_busted():
+        return -1
+    
     # loop through dealer preset decisions
-    # determine winner
-    # outcome = win, loss, put, etc.
-    # return outcome with all player decisions made in the game
+    ### delaer decision code or fucntion goes here
+    if dealer_hand.is_busted():
+        return 1
+    
+    # if neither busted, determine winner by comparing final 
+    # player hand and final dealer hand
+    if player_total == dealer_total:
+        return 0
+    if player_total > dealer_total:
+        return 1
+    if player_total < dealer_total:
+        return -1
+
+def generate_initial_game_state():
+    deck = Deck()
+    player_hand = Hand(deck.draw_card(2))
+    dealer_hand = Hand(deck.draw_card(2))
+    return deck, player_hand, dealer_hand
--- tests.py
+++ tests.py
@@ -1,33 +1,28 @@
 import blackjack_engine
 
-def test_is_busted_1():
-    """22, no aces"""
-    assert blackjack_engine.is_busted([13,5,7]) == True
+def test_sum_cards():
+    # 22, no aces
+    assert blackjack_engine.Hand([13,5,7]).sum_cards() == 22
+    # 19, no aces
+    assert blackjack_engine.Hand([10,9]).sum_cards() == 19
+    # soft 17
+    assert blackjack_engine.Hand([1,6]).sum_cards() == 17
+    # hard 17
+    assert blackjack_engine.Hand([1,12,6]).sum_cards() == 17
+    # hard 22 (ace=1)
+    assert blackjack_engine.Hand([1,12,6,5]).sum_cards() == 22
 
-def test_is_busted_2():
-    """19, no aces"""
-    assert blackjack_engine.is_busted([10,9]) == False
-
-def test_is_busted_3():
-    """soft 17"""
-    assert blackjack_engine.is_busted([1,6]) == False
-
-def test_is_busted_4():
-    """hard 17"""
-    assert blackjack_engine.is_busted([1,12,6]) == False
-
-def test_is_busted_5():
-    """hard 22 (ace=1)"""
-    assert blackjack_engine.is_busted([1,12,6,5]) == True
-
-def test_Deck_draw_card_1():
-    my_deck = blackjack_engine.Deck(1)
+def test_Deck_draw_card():
+    my_deck = blackjack_engine.Deck()
     assert len(my_deck.cards) == 52
     card = my_deck.draw_card()
-    assert card in [*range(1,13)]  # card is a valid integer
+    assert card in [*range(1,14)]  # card is a valid integer
     assert len(my_deck.cards) == 51  # 51 cards left after draw
-    assert my_deck.count(card) == 3 # only 3 of same card left in deck
-    card2 = my_deck.draw_card()
-    assert card2 in [*range(1,13)]  # card2 is a valid integer
-    assert len(my_deck.cards) == 50  # 50 cards left after second draw
-    assert my_deck.count(card2) < 4 # only 2 or 3 of card2 left in deck
+    assert my_deck.cards.count(card) == 3 # only 3 of same card left in deck
+    
+    cards2 = my_deck.draw_card(3)
+    assert len(cards2) == 3
+    assert len(my_deck.cards) == 48  # cards left after second draw
+    assert my_deck.cards.count(cards2[0]) < 4 # less than 4 of same card left in deck
+    assert my_deck.cards.count(cards2[1]) < 4 
+    assert my_deck.cards.count(cards2[2]) < 4 
