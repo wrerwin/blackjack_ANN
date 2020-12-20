37da795a06d986febf3b02002d5cd3cb74a0156b
Casey Brock <19877843+caseynbrock@users.noreply.github.com>
2020-12-20 13:53:38 -0500

    blackjack_engine overhaul to add BlackjackGame class

--- blackjack_engine.py
+++ blackjack_engine.py
@@ -1,5 +1,12 @@
 import random
 
+class GameOver(Exception):
+    pass
+
+class BustedHand(Exception):
+    pass
+
+
 class Deck():
     """currently initializes as a random deck of 52 cards"""
     def __init__(self):
@@ -19,12 +26,29 @@ class Deck():
         random.shuffle(deck)
         return deck
 
+
 class Hand():
+    """
+    attributes:
+        total: sum of all cards (highest possible value without busting)
+        is_soft: True if hand has an ace counted as 11, False otherwise
+        is_busted: True if total > 21
+        is_blackjack: true if only two cards and they are ace + face card
+    methods:
+        add_cards: add single card to hand and updates attributes
+
+    Don't try to remove cards. Instead, create a new Hand without the 
+    card you want to remove.
+    """
     def __init__(self, cards=None):
         if len(cards) < 2:
             raise ValueError("Initial hand must have at least two cards")
-        # do it this way so it uses validator in add_card
         self.cards = []
+        self.total = None
+        self.is_soft = None
+        self.is_busted = False
+        # self.is_blackjack = False
+        # do it this way so it uses validator in add_card
         for card in cards:
             self.add_card(card) 
 
@@ -35,87 +59,132 @@ class Hand():
         """
         if card not in [*range(1,14)]:
             raise ValueError("Not a valid card")
+
+        if self.is_busted == True:
+            raise BustedHand("Can't add a card to a busted hand")
+
         self.cards.append(card)
-
-    def sum_cards(self):
-        # need to account for aces
-        pass 
-
-    def is_busted(self):
-        """kinda unnecessary but whatever"""
-        return self.sum_cards() > 21
-
-    def is_soft(self):
-        """
-        return True if hand has ace being counted as 11
-        return False otherwise
-        """
-        # this is needed for dealer logic, which hits on soft 17 
+        print(self.cards)
+        if len(self.cards) > 1:
+            self.total, self.is_soft = self._sum_cards()
+            if self.total > 21:
+                self.is_busted = True
+ 
+    def _sum_cards(self):
+        # is_soft is needed for dealer logic, which hits on soft 17 
         # but stays on hard 17
-        pass
+        card_values = {1:11, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7,
+                       8:8, 9:9, 10:10, 11:10, 12:10, 13:10}
+        card_values_hard = card_values.copy()
+        card_values_hard[1] = 1
 
-def blackjack_round():
+        is_soft = False
+        total = sum([card_values[card] for card in self.cards])
+        # if self.total > 21 and 1 in self.cards:
+        #     total = sum([card_values_hard[card] for card in self.cards])
+        return total, is_soft
+
+
+class BlackjackGame():
     """ 
-    Results:
+    The results are defined as ints based on how much money they make,
+    a multiplier of the orignal bet.
         lose: -1
         push: 0
         win: 1
         blackjack: 1.5
+
+    Attributes (read only):
+        player_hand (Hand): current player hand
+        dealer_hand (Hand): current dealer hand
+        dealer_upcard (int): dealer upcard, which is dealer_hand[0]
+        is_finished (boolean): whether the game is finished
+        result (int): result of the game is -1, 0, 1, or 1.5.
+            Or None if game not finished.
+        deck (Deck): current deck
+
+    Methods:
+        player_move (need better name?): update the game after player decision
     """
-    deck, player_hand, dealer_hand = generate_initial_game_state()
-    dealer_upcard = dealer_hand[1]  # dealer's second card
-
-    # check if player or dealer have blackjack
-    player_blackjack = dealer_blackjack = False
-    if player_hand.sum_cards == 21:
-        player_blackjack = True
-    if dealer_hand.sum_cards == 21:
-        dealer_blackjack = True
-    if player_blackjack and dealer_blackjack:
-        return 0
-    elif player_blackjack:
-        return 1.5
-    elif dealer_blackjack:
-        return -1
-
-    # loop through player decisions until stay or bust
-    while True:
-        hit_or_stay == random.choice(['hit', 'stay'])
-        if hit_or_stay == 'stay':
-            break
-        if hit_or_stay == 'hit':
-            player_hand.add_card(deck.draw_card())
-        if player_hand.is_busted():
+    def __init__(self):
+        self.deck = Deck()
+        self.player_hand = Hand(deck.draw_card(2))
+        self.dealer_hand = Hand(deck.draw_card(2))
+        self.dealer_upcard = self.dealer_hand[0]
+        self.is_finished = False
+        self.result = None
+        if cfb_result := self._check_for_blackjacks() is not None:
+            self.is_finished = True
+            self.result = cfb_result
+    
+    def _check_for_blackjacks(self):
+        """
+        checks if player, dealer, or both have blackjack
+        """
+        player_blackjack = dealer_blackjack = False
+        if self.player_hand.sum_cards == 21:
+            player_blackjack = True
+        if self.dealer_hand.sum_cards == 21:
+            dealer_blackjack = True
+        if player_blackjack and dealer_blackjack:
+            return 0
+        elif player_blackjack:
+            return 1.5
+        elif dealer_blackjack:
             return -1
+        else:
+            return None
     
-    # loop through dealer preset decisions
-    while True:
-        hit_or_stay = dealer_bot(dealer_hand)
+    def player_move(self, hit_or_stay):
+        """ haven't implemented double or split 
+        player can't hit on 21
+        """
+        if self.is_finished == True:
+            raise GameOver("This game is over. Can't make a move.")
+
         if hit_or_stay == 'stay':
-            break
-        if hit_or_stay == 'hit':
-            dealer_hand.add_card(deck.draw_card())
-        if dealer_hand.is_busted():
-            return 1
+            self._dealer_turn()
+        elif hit_or_stay == 'hit':
+            player_hand.add_card(deck.draw_card())
+            # check for game status changes after move
+            if player_hand.is_busted():
+                self._finish_game(-1)
+            elif player_hand.total == 21:
+                # in this case, game isn't over but player is done
+                self._dealer_turn() # updates dealer hand
+            elif player_hand.total < 21:
+                pass
+        
+    def _dealer_turn(self):
+        # loop through dealer preset decisions
+        # finishes game once dealer is done
+        while True:
+            hit_or_stay = dealer_bot(dealer_hand)
+            if hit_or_stay == 'stay':
+                break
+            if hit_or_stay == 'hit':
+                dealer_hand.add_card(deck.draw_card())
+            if dealer_hand.is_busted() or dealer_hand.total == 21:
+                break
+        
+        # Assuming dealer didn't bust, determine winner by comparing final 
+        # player hand and final dealer hand
+        if player_total == dealer_total:
+            self._finish_game(0)
+        if player_total > dealer_total:
+            self.finish_game(1)
+        if player_total < dealer_total:
+            self._finish_game(-1)
+
+    def _finish_game(self, result):
+        self.is_finished = True
+        self.result = result
+
+
     
-    # if neither busted, determine winner by comparing final 
-    # player hand and final dealer hand
-    if player_total == dealer_total:
-        return 0
-    if player_total > dealer_total:
-        return 1
-    if player_total < dealer_total:
-        return -1
-
-def generate_initial_game_state():
-    deck = Deck()
-    player_hand = Hand(deck.draw_card(2))
-    dealer_hand = Hand(deck.draw_card(2))
-    return deck, player_hand, dealer_hand
-
 def dealer_bot(dealer_hand):
     """
-    Make a preset decisions for dealer given current hand.
+    Make preset decisions for dealer given current hand.
     Currently hits on soft 17
 
     returns "hit" or "stay" 
--- tests.py
+++ tests.py
@@ -1,55 +1,25 @@
 import blackjack_engine
 import pytest
 
-def test_sum_cards_22_no_aces():
-    assert blackjack_engine.Hand([13,5,7]).sum_cards() == 22
+# Hand tests
+def test_Hand_total_22_no_aces():
+    assert blackjack_engine.Hand([13,5,7]).total == 22
 
-def test_sum_cards_19_no_aces():
-    assert blackjack_engine.Hand([10,9]).sum_cards() == 19
+def test_Hand_total_19_no_aces():
+    assert blackjack_engine.Hand([10,9]).total == 19
 
-def test_sum_cards_soft_17():
-    assert blackjack_engine.Hand([1,6]).sum_cards() == 17
+def test_Hand_total_soft_17():
+    assert blackjack_engine.Hand([1,6]).total == 17
 
-def test_sum_cards_hard_17():
-    assert blackjack_engine.Hand([1,12,6]).sum_cards() == 17
+def test_Hand_total_hard_17():
+    assert blackjack_engine.Hand([1,12,6]).total == 17
 
-def test_sum_cards_hard_22():
-    assert blackjack_engine.Hand([1,12,6,5]).sum_cards() == 22
+def test_Hand_total_hard_22():
+    assert blackjack_engine.Hand([1,12,6,5]).total == 22
 
-def test_Deck_draw_card():
-    my_deck = blackjack_engine.Deck()
-    assert len(my_deck.cards) == 52
-    card = my_deck.draw_card()
-    assert card in [*range(1,14)]  # card is a valid integer
-    assert len(my_deck.cards) == 51  # 51 cards left after draw
-    assert my_deck.cards.count(card) == 3 # only 3 of same card left in deck
-    
-    cards2 = my_deck.draw_card(3)
-    assert len(cards2) == 3
-    assert len(my_deck.cards) == 48  # cards left after second draw
-    assert my_deck.cards.count(cards2[0]) < 4 # less than 4 of same card left in deck
-    assert my_deck.cards.count(cards2[1]) < 4 
-    assert my_deck.cards.count(cards2[2]) < 4 
-
-def test_dealer_bot_16():
-    hand = blackjack_engine.Hand([11,6])
-    assert blackjack_engine.dealer_bot(hand) == 'hit'
-
-def test_dealer_bot_hard_17():
-    hand = blackjack_engine.Hand([11,6,1])
-    assert blackjack_engine.dealer_bot(hand) == 'stay'
-
-def test_dealer_bot_soft_17():
-    hand = blackjack_engine.Hand([1,6])
-    assert blackjack_engine.dealer_bot(hand) == 'hit'
-
-def test_dealer_bot_20():
-    hand = blackjack_engine.Hand([12,4,6])
-    assert blackjack_engine.dealer_bot(hand) == 'stay'
-
-def test_dealer_bot_hard_20():
-    hand = blackjack_engine.Hand([1,2,12,7])
-    assert blackjack_engine.dealer_bot(hand) == 'stay'
+def test_Hand_total_two_aces():
+    # one ace is hard and one is soft
+    assert False
 
 def test_Hand_is_soft_no_aces():
     hand = blackjack_engine.Hand([11,6])
@@ -80,3 +50,46 @@ def test_Hand_add_card_badcard():
         hand.add_card(1.5)
     with pytest.raises(ValueError):
         hand.add_card('cat')
+
+def test_Hand_add_card_busted():
+    hand = blackjack_engine.Hand([1,2,12,13])
+    with pytest.raises(BustedHand):
+        hand.add_card(5)
+
+# Deck tests
+def test_Deck_draw_card():
+    my_deck = blackjack_engine.Deck()
+    assert len(my_deck.cards) == 52
+    card = my_deck.draw_card()
+    assert card in [*range(1,14)]  # card is a valid integer
+    assert len(my_deck.cards) == 51  # 51 cards left after draw
+    assert my_deck.cards.count(card) == 3 # only 3 of same card left in deck
+    
+    cards2 = my_deck.draw_card(3)
+    assert len(cards2) == 3
+    assert len(my_deck.cards) == 48  # cards left after second draw
+    assert my_deck.cards.count(cards2[0]) < 4 # less than 4 of same card left in deck
+    assert my_deck.cards.count(cards2[1]) < 4 
+    assert my_deck.cards.count(cards2[2]) < 4 
+
+# Dealer bot tests
+# dealer bot tests will fail if Hand tests fail unfortunately
+def test_dealer_bot_16():
+    hand = blackjack_engine.Hand([11,6]) 
+    assert blackjack_engine.dealer_bot(hand) == 'hit'
+
+def test_dealer_bot_hard_17():
+    hand = blackjack_engine.Hand([11,6,1])
+    assert blackjack_engine.dealer_bot(hand) == 'stay'
+
+def test_dealer_bot_soft_17():
+    hand = blackjack_engine.Hand([1,6])
+    assert blackjack_engine.dealer_bot(hand) == 'hit'
+
+def test_dealer_bot_20():
+    hand = blackjack_engine.Hand([12,4,6])
+    assert blackjack_engine.dealer_bot(hand) == 'stay'
+
+def test_dealer_bot_hard_20():
+    hand = blackjack_engine.Hand([1,2,12,7])
+    assert blackjack_engine.dealer_bot(hand) == 'stay'
